090f35d39f8fa1eb4ac0bbcb665c8ce0773197e2
Khilee01 <100354343+Khilee01@users.noreply.github.com>
2022-02-24 18:46:29 +0530

    product .c++
    Write a C++ Program to find the product of two numbers entered by the User Your Output should look something like this Enter two numbers: 3.4 5.5 Product = 18.7

---
+++ Khilee ass 1 part 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>    
+using namespace std;
+
+int main()     
+{
+ int x,y;
+ 
+ cout<<"Enter x"<<endl;
+ cin>>x;
+ cout<<"Enter y"<<endl;
+ cin>>y;
+ cout<<x*y<<endl;
+ return 0;
+ }
