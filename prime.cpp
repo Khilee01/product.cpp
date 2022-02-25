5434520f039a70d74d790b18b5a18266d889f0d4
Khilee01 <100354343+Khilee01@users.noreply.github.com>
2022-02-26 00:47:43 +0530

    prime.cpp
    Write a C++ program to Check Whether a Number can be Express as Sum of Two Prime Numbers

---
+++ prime.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+bool check_prime(int n);
+
+int main() {
+
+  int n, i;
+  bool flag = false;
+
+  cout << "Enter a positive  integer: ";
+  cin >> n;
+
+  for(i = 2; i <= n/2; ++i) {
+    if (check_prime(i)) {
+      if (check_prime(n - i)) {
+        cout << n << " = " << i << " + " << n-i << endl;
+        flag = true;
+      }
+    }
+  }
+
+  if (!flag)
+    cout << n << " can't be expressed as sum of two prime numbers.";
+
+  return 0;
+}
