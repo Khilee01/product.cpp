57ad6ff264e35c9cc0efe94b38e1ef60eb5e5b7d
Khilee01 <100354343+Khilee01@users.noreply.github.com>
2022-02-25 17:26:06 +0530

    vote.cpp
    vote

---
+++ vote.cpp
@@ -0,0 +1,14 @@
+#include <iostream>    
+using namespace std;
+
+int main()     
+{ 
+    int age;
+   cout<<"Enter the age:";
+   cin>>age;
+   if (age>=18)
+   {cout<<endl"you are elligible for voting";}
+   else
+   {cout<<"you are not eligible to vote currently wait for"<<18-age<<"year(s)";}
+   retun 0;
+   }
